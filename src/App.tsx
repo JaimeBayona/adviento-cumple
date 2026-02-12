65a09fc210331e58bce263fa4c93991a251b3c76
Jaime Bayona <joelbayona06@gmail.com>
2026-02-11 20:56:38 -0500

    fix: update package-lock.json, package.json, and src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Home from "./pages/Home"
+import { Analytics } from "@vercel/analytics/react"
 
 import "./index.css"
 
 export default function App() {
   return (
-      
+      <>
       <Home/>
+
+      <Analytics />
+      </>
   )
 }
